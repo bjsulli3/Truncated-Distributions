70130b7995624a37f7de22e4a927fd0374025952
bjsulli3 <54413894+bjsulli3@users.noreply.github.com>
2020-03-01 20:27:40 -0500

    Update trunc.py

--- trunc.py
+++ trunc.py
@@ -2,7 +2,11 @@
 
 from numpy import append, NINF, Inf
 
-def trunc(dist, size=1, lower=NINF, upper=Inf):
+def trunc(dist, size=1, lower=NINF, upper=Inf, ignore_warning=False):
+    
+    if lower > upper:
+        raise ValueError('lower=' + str(lower) + ' must be less than upper=' +str(upper))
+    
     stringDist = dist[0:-1] + ', size=' + str(size) + ')'
     x = eval(stringDist)
     
@@ -10,6 +14,7 @@ def trunc(dist, size=1, lower=NINF, upper=Inf):
     
     truncated = x[oob]  #in bounds elements
     
+    i=0
     while len(truncated) < size:
         y = eval(stringDist)
         oob = (y < lower) | (y > upper) == False
@@ -17,10 +22,16 @@ def trunc(dist, size=1, lower=NINF, upper=Inf):
         newTruncated = y[oob]
         
         truncated = append(truncated, newTruncated)
-    
+        
+        i += 1
+
+        if i == 10000 and ignore_warning == False:
+            raise RuntimeError('ensure shape and bound parameters are reasonable')
+            
     truncated = truncated[0:size]
         
     return truncated
 
+trunc('scipy.stats.uniform.rvs(loc=10, scale=10)', lower=23, upper=22, size=1000)
 
-
+scipy.stats.uniform.rvs(loc=10, scale=10)
